ee0863fcfa3b67aab7311729bf158b90e593fb35
Francesc Requesens <francesc.requesens@gmail.com>
2020-04-25 20:47:39 +0200

    Sample

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from kaa.kaa import Kaa
 
 def start_server():
     kaa = Kaa()
-    kaa.registerFilterRequest(module='filters', className='OnRequest')
-    kaa.registerResources(module='resources',  className='Resources')
-    kaa.registerFilterResponse(module='filters', className='OnResponse')
+    kaa.registerFilterRequest('sample.filters', 'OnRequest')
+    kaa.registerResources('sample.resources',  'Resources')
+    kaa.registerFilterResponse('sample.filters', 'OnResponse')
     kaa.run()
 
 start_server()
