2c71ceb56d57a654fc83faae9970911e443a8b5a
Francesc Requesens <francesc.requesens@gmail.com>
2020-04-26 18:55:19 +0200

    Fixed import module

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from kaa.kaa import Kaa
+from kaa import Kaa
 
 
 def application(env, start_response):
@@ -6,4 +6,5 @@ def application(env, start_response):
     kaa.registerResources('sample.resources', 'Resources')
     kaa.registerFilterRequest('sample.filters', 'OnRequest')
     kaa.registerFilterResponse('sample.filters', 'OnResponse')
+    kaa.registerFilterResponse('sample.filters', 'EnableCors')
     return kaa.serve()
