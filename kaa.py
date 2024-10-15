397ecdf04ae710feb07a60c9d2a2d1d3c05ce99c
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-10-15 19:22:23 +0200

    feat: remove legacy code

--- kaa.py
+++ kaa.py
@@ -1,11 +1,5 @@
 from kaa.cli import Cli, Server
 
-
 # For WSGI application
 def application(env, start_response):
     return Server().serve(env, start_response)
-
-
-if __name__ == "__main__":
-    cli = Cli()
-    cli.execute()
